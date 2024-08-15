052b3bffe23bebf95938b35e92a3af663c03d1b9
Fernando <vluisfernando62@gmail.com>
2024-08-15 17:18:30 -0400

    fix: Correct the ProductList page and make changes to the product card

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,29 +1,24 @@
-import { useRoutes } from "react-router-dom";
-import ProductList from "./features/ProductCatalog/pages/ProductList";
-import ProductDetail from "./features/ProductCatalog/pages/ProductDetail";
-import NotFound from "./shared/pages/NotFound";
-import ProductList2 from "./features/ProductCatalog/pages/ProductList2";
+import { useRoutes } from 'react-router-dom';
+import ProductList from './features/ProductCatalog/pages/ProductList';
+import ProductDetail from './features/ProductCatalog/pages/ProductDetail';
+import NotFound from './shared/pages/NotFound';
 
 const Routes = () => {
-    let routes = useRoutes([
-        {
-            path: "/",
-            element: <ProductList />
-        },
-        {
-            path: "/details",
-            element: <ProductDetail />
-        },
-        {
-            path: "/*",
-            element: <NotFound />
-        },
-        {
-            path: "/list",
-            element: <ProductList2 />
-        }
-    ]);
-    return routes;
-}
+	const routes = useRoutes([
+		{
+			path: '/',
+			element: <ProductList />
+		},
+		{
+			path: '/details',
+			element: <ProductDetail />
+		},
+		{
+			path: '/*',
+			element: <NotFound />
+		}
+	]);
+	return routes;
+};
 
 export default Routes;
