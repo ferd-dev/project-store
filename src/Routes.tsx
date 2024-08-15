2c905de15fd96f2ef987b625b2b0e10ecb7cb2b0
brayan146 <brayan14717@gmail.com>
2024-08-15 11:08:20 -0400

    Product cards

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import { useRoutes } from "react-router-dom";
 import ProductList from "./features/ProductCatalog/pages/ProductList";
 import ProductDetail from "./features/ProductCatalog/pages/ProductDetail";
 import NotFound from "./shared/pages/NotFound";
+import ProductList2 from "./features/ProductCatalog/pages/ProductList2";
 
 const Routes = () => {
     let routes = useRoutes([
@@ -16,6 +17,10 @@ const Routes = () => {
         {
             path: "/*",
             element: <NotFound />
+        },
+        {
+            path: "/list",
+            element: <ProductList2 />
         }
     ]);
     return routes;
