0a76ca00141dab6f1bad70f241f87dc08d0d3232
Fernando <vluisfernando62@gmail.com>
2024-09-02 11:18:32 -0400

    improve purchase alert

--- src/features/ShoppingCart/pages/ShoppingCart.tsx
+++ src/features/ShoppingCart/pages/ShoppingCart.tsx
@@ -3,17 +3,32 @@ import Layout from '../../../shared/components/Layout';
 import { CardContext } from '../../../shared/context/CardContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import Swal from 'sweetalert2';
 
 const ShoppingCart = () => {
   const context = useContext(CardContext);
 
   const handlePayment = () => {
-    // Lógica para manejar el pago simulado
-    alert('Pago realizado con éxito');
-    // Redirigir a una página de confirmación de pago
+    Swal.fire({
+      title: 'Are you sure you want to make the payment?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, pay',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Payment made successfully',
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }
+    });
   };
 
-  // Calcular el total a pagar
   const totalAmount = context?.products?.reduce((total, product) => {
     return total + product.price * product.amount;
   }, 0);
@@ -34,7 +49,9 @@ const ShoppingCart = () => {
                   className="w-20 h-20 object-cover rounded-lg shadow-md"
                 />
                 <div>
-                  <h3 className="text-lg font-semibold text-gray-900">{product.title}</h3>
+                  <h3 className="text-lg font-semibold text-gray-900">
+                    {product.title}
+                  </h3>
                   <p className="text-gray-500">$ {product.price}</p>
                 </div>
               </div>
@@ -59,7 +76,7 @@ const ShoppingCart = () => {
           ))}
           <div className="flex justify-end mt-4">
             <div className="text-lg font-semibold text-gray-900">
-              Total a pagar: $ {totalAmount?.toFixed(2)}
+              Total to pay: $ {totalAmount?.toFixed(2)}
             </div>
           </div>
           <div className="flex justify-end mt-4">
@@ -67,7 +84,7 @@ const ShoppingCart = () => {
               onClick={handlePayment}
               className="inline-flex items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
-              Realizar Pago
+              Make Payment
             </button>
           </div>
         </div>
